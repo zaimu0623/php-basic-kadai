ad50f359a97ebddfa8bbf8467111b41005265500
zaimu0623 <seibu.saiki0316@gmail.com>
2024-11-25 02:21:02 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+
+
+ <body>
+     <p>
+         <?php
+         // 連想配列に値を代入する
+         $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+ 
+         // 連想配列の値を出力する
+         print_r($product_data);
+         ?>
+      </p>
+
+
+ </body>
+ </html>
